52a6a88fc9511760c7f61d77667133d7d92ec525
Ging <YanM16M@gmail.com>
2024-05-29 12:42:08 +0200

    update navbar's name

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
     return (
         <nav className="fixed top-0 w-full flex items-center justify-between max-sm:justify-center px-6 py-8 z-[50]">
             <Link to="/" onClick={onClick} className="max-sm:hidden font-medium text-3xl drop-shadow-md">
-                Portfolio
+                Yanis Martinon
             </Link>
             <div className="flex items-center gap-2 ">
                 <Button variant="outline" className={cn(
