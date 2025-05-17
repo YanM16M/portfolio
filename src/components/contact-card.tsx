889229c6116eead6118fd8a7d5457df71b5759e6
Ging <YanM16M@gmail.com>
2025-05-17 18:27:48 +0200

    add follows

--- src/components/contact-card.tsx
+++ src/components/contact-card.tsx
@@ -1,5 +1,10 @@
-import { ExternalLink } from "lucide-react";
+import { useMutation } from "convex/react";
+
 import { Link } from "react-router-dom";
+
+import { api } from "../../convex/_generated/api";
+
+import { ExternalLink } from "lucide-react";
 
 interface ContactCardProps {
     title: string;
@@ -12,21 +17,34 @@ export const ContactCard = ({
     title,
     description,
     icon,
-    url
+    url,
 }: ContactCardProps) => {
+    const addFollow = useMutation(api.follows.addFollows);
+
+    const handleOnClick = () => {
+        addFollow({
+            clickOn: description || url,
+        });
+    };
+
     return (
-        <Link to={url} target="_blank" className="group w-full max-w-xl flex items-center justify-between p-4 border rounded-lg shadow-sm transition
-        hover:-translate-y-2 hover:cursor-pointer">
+        <Link
+            onClick={handleOnClick}
+            to={url}
+            target="_blank"
+            className="group w-full max-w-xl flex items-center justify-between p-4 border rounded-lg shadow-sm transition
+        hover:-translate-y-2 hover:cursor-pointer"
+        >
             <div className="flex items-center gap-2">
-                <div className="p-4 border rounded-full">
-                   {icon}
-                </div>
+                <div className="p-4 border rounded-full">{icon}</div>
                 <div className="flex flex-col">
                     <span className="font-semibold">{title}</span>
-                    <span className="text-xs text-muted-foreground">{description}</span>
+                    <span className="text-xs text-muted-foreground">
+                        {description}
+                    </span>
                 </div>
             </div>
             <ExternalLink className={`group-hover:opacity-75 transition`} />
         </Link>
-    )
-}
+    );
+};
--- src/pages/Home/_components/WelcomeSection.tsx
+++ src/pages/Home/_components/WelcomeSection.tsx
@@ -1,17 +1,32 @@
 import cv from "@/assets/CV.pdf";
 
+import { api } from "../../../../convex/_generated/api";
+
 import { Download, Linkedin } from "lucide-react";
 
 import { GridBackground } from "@/components/ui/GridBackground";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "convex/react";
 
 export const WelcomeSection = () => {
+    const addFollows = useMutation(api.follows.addFollows);
+
+    const handleClick = (title: string) => {
+        addFollows({
+            clickOn: title,
+        });
+    };
 
     return (
-        <section id="welcome" className="h-full flex flex-col gap-y-3 items-center justify-center">
+        <section
+            id="welcome"
+            className="h-full flex flex-col gap-y-3 items-center justify-center"
+        >
             <GridBackground className="flex flex-col gap-y-3 items-center justify-center">
-                <h1 className="text-5xl text-white text-center font-anton text-stroke uppercase leading-tight tracking-wide
-                dark:text-white">
+                <h1
+                    className="text-5xl text-white text-center font-anton text-stroke uppercase leading-tight tracking-wide
+                dark:text-white"
+                >
                     Hello, I'm&nbsp;
                     <span className="text-primary text-stroke-none">
                         Yanis Martinon
@@ -21,13 +36,24 @@ export const WelcomeSection = () => {
                     I am a frontend and backend developer
                 </span>
                 <div className="space-x-2">
-                    <Button className="font-semibold bg-blue-500 hover:bg-blue-500/90" asChild>
-                        <a href="https://www.linkedin.com/in/yanis-martinon-381651194/" target="_blank">
+                    <Button
+                        onClick={() => handleClick("LinkedIn")}
+                        className="font-semibold bg-blue-500 hover:bg-blue-500/90"
+                        asChild
+                    >
+                        <a
+                            href="https://www.linkedin.com/in/yanis-martinon-381651194/"
+                            target="_blank"
+                        >
                             LinkedIn
                             <Linkedin className="ml-2" />
                         </a>
                     </Button>
-                    <Button asChild className="font-semibold">
+                    <Button
+                        onClick={() => handleClick("Download CV")}
+                        asChild
+                        className="font-semibold"
+                    >
                         <a href={cv} target="_blank">
                             Download CV
                             <Download className="ml-2" />
@@ -36,5 +62,5 @@ export const WelcomeSection = () => {
                 </div>
             </GridBackground>
         </section>
-    )
-}
+    );
+};
