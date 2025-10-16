50bbbba94b90c5f9d06a385aa36f7f71efe40511
Ging <YanM16M@gmail.com>
2025-10-16 12:20:33 +0200

    update

--- convex/follows.ts
+++ convex/follows.ts
@@ -8,32 +8,6 @@ export const addFollows = mutation({
 
         if (!clickOn) return;
 
-        // Début de la journée
-        const startOfDay = new Date();
-        startOfDay.setHours(0, 0, 0, 0);
-        const startTimestamp = startOfDay.getTime();
-
-        // Début du lendemain (fin de la journée d'aujourd'hui)
-        const endOfDay = new Date(startOfDay);
-        endOfDay.setDate(endOfDay.getDate() + 1);
-        const endTimestamp = endOfDay.getTime();
-
-        const alreadyExist = await ctx.db
-            .query("follows")
-            .filter((q) =>
-                q.and(
-                    q.eq(q.field("userId"), userId),
-                    q.eq(q.field("clickOn"), "/"),
-                    q.gte(q.field("_creationTime"), startTimestamp),
-                    q.lt(q.field("_creationTime"), endTimestamp)
-                )
-            )
-            .first();
-
-        if (alreadyExist) {
-            return alreadyExist;
-        }
-
         const newFollows = await ctx.db.insert("follows", {
             userId,
             clickOn,
